3c7fd6b92156e0afd1d99be538636670d9febe9e
Ryanandpsp <r_liu_321@hotmail.com>
2020-07-16 17:22:48 -0400

    python script tests

---
+++ GeniusLyricPythonScript/lg_packagetest.py
@@ -0,0 +1,4 @@
+import lyricsgenius
+genius = lyricsgenius.Genius("RTg0Rc35PsaeBR2FUyTWxA2fttjlZ4iZZFKgI3TkIsokIfZH-eFScaPz8Icnd4VM")
+artist = genius.search_artist("Andy Shauf", max_songs=3, sort="title")
+print(artist.songs)
---
+++ GeniusLyricPythonScript/scripttest.py
@@ -0,0 +1,11 @@
+import sys
+def main(argv):
+    print (argv[1])
+    return 3
+
+
+    
+if __name__== '__main__':
+    main(sys.argv)
+
+
