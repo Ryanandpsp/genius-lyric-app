77e8b846cc51ccf81157b6deea4651146a2af634
Ryanandpsp <r_liu_321@hotmail.com>
2020-08-12 20:07:05 -0400

    Started function for the authorization process for spotify API, created server for extracting authorization code.

--- GeniusLyricPythonScript/spotifycurrent.py
+++ GeniusLyricPythonScript/spotifycurrent.py
@@ -2,16 +2,32 @@ import requests
 import urllib.parse as urlparse
 import webbrowser
 
+
+
 client_id = "8e8a7f221fa84c71a6c31e5eb830bea5"
 # client_secret =
 redirect_uri = "https://www.google.ca/"
 
+scopeList = ["user-read-currently-playing", "user-read-playback-state"]
+
 payload = {"client_id": "8e8a7f221fa84c71a6c31e5eb830bea5",
            "response_type": "code",
-           "redirect_uri": "https://www.google.ca/",
-           "scope[]": ["user-read-currently-playing", "user-read-playback-state"]}
-urlparam = urlparse.urlencode(payload)
-authurl = "%s?%s" % ("https://accounts.spotify.com/authorize", urlparam)
-webbrowser.open(authurl)
-#response = requests.get("https://accounts.spotify.com/authorize")
-print(authurl)
+           "redirect_uri": "http://localhost:5050" }
+
+payload["state"] = scopeList
+
+def get_auth_url(payload):
+    urlparam = urlparse.urlencode(payload)
+    authurl = "%s?%s" % ("https://accounts.spotify.com/authorize", urlparam)
+    return authurl
+
+
+webbrowser.open(get_auth_url(payload))
+
+
+
+
+
+
+
+
