b6def17d8040a076009ccda0a6596110af817902
Ryanandpsp <r_liu_321@hotmail.com>
2020-10-02 14:08:42 -0400

    auth server now automatically exchanges for access token

--- GeniusLyricApp/NodejsTestWebApp1/auth-server.js
+++ GeniusLyricApp/NodejsTestWebApp1/auth-server.js
@@ -1,8 +1,9 @@
 'use strict';
 var http = require('http');
 var url = require('url');
+var request = require("request");
 var rh = require("./request-handler");
-;
+
 
 var port = process.env.PORT || 1337;
 var clientid = process.env.SPOTIFY_CLIENT_ID;
@@ -22,14 +23,39 @@ var server = http.createServer(function (req, res) {
     
     var requrl = new URL(req.url, `http://${req.headers.host}`);
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    console.log(requrl.pathname)
+    console.log(requrl.pathname);
+
     if (requrl.pathname === "/authorize") {
         //redirect to spotify for authorization
         res.writeHead(301, { Location: rh.getAuthUrl(payload, AuthBaseUrl).toString()});
         res.end();
     }
+
     else if (requrl.pathname === "/callback") {
         //get auth code and exchange for access token then redirect
+        let urlparams = requrl.searchParams;
+        let code = urlparams.get("code");
+        console.log(code);
+        let authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+                code: code,
+                redirect_uri: "http://192.168.0.9:1337/callback",
+                grant_type: 'authorization_code'
+            },
+            headers: {
+                'Authorization': 'Basic ' + (new Buffer(
+                    process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
+                ).toString('base64'))
+            },
+            json: true
+        }
+        
+        request.post(authOptions, function (error, response, body) {
+            var access_token = body.access_token;
+            console.log(access_token);
+        })
+        
         res.end('Redirecting\n');
     }
     else { res.end('Hello World\n');}
